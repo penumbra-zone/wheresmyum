a9177b8319eaeef3d664eb1b5e20f2e52f03652a
Lucas Meier <lucas@cronokirby.com>
2024-12-03 19:39:04 -0800

    Link up database and frontend display
    This adds a complete dummy display, but shows E2E flows.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,9 +3,28 @@ import { Density } from "@penumbra-zone/ui/Density";
 import { Display } from "@penumbra-zone/ui/Display";
 import { Text } from "@penumbra-zone/ui/Text";
 import { MetaFunction, useLoaderData } from "@remix-run/react";
+import { Database, db } from "backend/database";
+import Transfer from "shared/Transfer";
 
-export const loader = async (): Promise<string> => {
-  return "hello world!";
+class Data {
+  constructor(public transfers: Transfer[]) {}
+
+  static async fetch(db: Database): Promise<Data> {
+    const [transfers] = await Promise.all([Transfer.fetchMany(db, 100)]);
+    return new Data(transfers);
+  }
+
+  toJSON(): Jsonified<Data> {
+    return { transfers: this.transfers.map((x) => x.toJSON()) };
+  }
+
+  static fromJSON(data: Jsonified<Data>): Data {
+    return new Data(data.transfers.map((x) => Transfer.fromJSON(x)));
+  }
+}
+
+export const loader = async (): Promise<Data> => {
+  return await Data.fetch(db);
 };
 
 export const meta: MetaFunction = () => {
@@ -13,12 +32,12 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  const raw = useLoaderData<Jsonified<string>>();
+  const raw = useLoaderData<Jsonified<Data>>();
   return (
     <Display>
       <Density compact>
         <Text h1 color="text.primary">
-          {raw}
+          {JSON.stringify(raw)}
         </Text>
       </Density>
     </Display>
--- backend/database/schema.ts
+++ backend/database/schema.ts
@@ -1,1 +1,22 @@
-export interface Schema {}
+export interface Schema {
+  wmu_transfers: WmuTransfers;
+  block_details: BlockDetails;
+}
+
+export interface WmuTransfers {
+  rowid: number;
+  channel: string;
+  sequence: bigint;
+  asset_id: Uint8Array;
+  amount: string;
+  sender: Uint8Array;
+  receiever: string;
+  height: bigint;
+  tx?: Uint8Array;
+}
+
+export interface BlockDetails {
+  height: bigint;
+  root: Uint8Array;
+  timestamp: Date;
+}
